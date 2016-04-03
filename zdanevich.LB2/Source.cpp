d5db0641eb4370ead4538f62c44273a7fcd46028
Oleg Zdanevic <ya.zdanevich@yandex.ru>
2016-04-03 20:08:43 +0300

    Update Source.cpp

--- zdanevich.LB2/Source.cpp
+++ zdanevich.LB2/Source.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-double** AllocMatrix(int);
+
 double** Difference(double**, double**, int);
 double CosMy(double, double);
+double MaxElement(double**, int);
+inline double** AllocMatrix(int);
+inline double MaxElement(double*, int);
+inline double PeriodDown(double);
 void FillMatrixMyFunction(double**, int, double);
 void FillMatrixNotMyFunction(double**, int);
 void DisplayMatrix(double**, int);
-void ClearMemory(double**, int);
+inline void ClearMemory(double**, int);
 
 
-
+double pi = 3.14159265359;
 
 
 
@@ -43,6 +47,11 @@ int main()
 		cout << endl << "difference" << endl << endl;
 		double **c = Difference(a, b, n);
 		DisplayMatrix(c, n);
+		cout << endl << endl << "*******************************************************************************" << endl << endl;
+		double max = MaxElement(c, n);
+		cout << "Maximum element in matrix= " << max << endl;
+		cout << "Ebsilon= " << eps << endl;
+		cout << endl;
 		system("pause");
 		system("cls");
 		ClearMemory(a, n);
@@ -63,15 +72,7 @@ int main()
 	return 0;
 }
 
-double** AllocMatrix(int n)
-{
-	double**a = new double*[n];
-	for (int i = 0; i < n; i++)
-	{
-		a[i] = new double[n];
-	}
-	return a;
-}
+
 
 double** Difference(double**a, double**b, int n)
 {
@@ -80,7 +81,7 @@ double** Difference(double**a, double**b, int n)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			c[i][j] = a[i][j] - b[i][j];
+			c[i][j] = fabs(a[i][j] - b[i][j]);
 		}
 	}
 	return c;
@@ -100,6 +101,43 @@ double CosMy(double x, double eps)
 	return sum;
 }
 
+double MaxElement(double**matr, int n)
+{
+	double max = MaxElement(matr[0], n);
+	for (int i = 1; i < n; i++)
+	{
+		double strmax = MaxElement(matr[i], n);
+		if (strmax>max) max = strmax;
+	}
+	return max;
+}
+
+inline double** AllocMatrix(int n)
+{
+	double**a = new double*[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = new double[n];
+	}
+	return a;
+}
+
+
+inline double MaxElement(double*arr, int n)
+{
+	double max = arr[0];
+	for (int j = 1; j < n; j++)
+	{
+		if (arr[j]>max) max = arr[j];
+	}
+	return max;
+}
+
+inline double PeriodDown(double rad)
+{
+	while (rad > pi) rad -= 2* pi;
+	return rad;
+}
 
 void FillMatrixMyFunction(double **a, int n, double eps)
 {
@@ -109,7 +147,7 @@ void FillMatrixMyFunction(double **a, int n, double eps)
 		{
 			if (j != i)
 			{
-				a[i][j] = (pow(i + j, 2) - CosMy(2 * i, eps) + CosMy(2 * j, eps)) / (CosMy(i + j, eps) + pow(i + 1, 2));
+				a[i][j] = (pow(i + j+2, 2) - CosMy(PeriodDown(2*i+2), eps) + CosMy(PeriodDown(2 * j+2), eps)) / (CosMy(PeriodDown(i+j+2), eps) + pow(i + 2, 2));
 			}
 			else a[i][j] = 2 * i;
 		}
@@ -124,7 +162,7 @@ void FillMatrixNotMyFunction(double**b, int n)
 		{
 			if (j != i)
 			{
-				b[i][j] = (pow(i + j, 2) - cos(2 * i) + cos(2 * j)) / (cos(i + j) + pow(i + 1, 2));
+				b[i][j] = (pow(i + j+2, 2) - cos(2 * i+2) + cos(2 * j+2)) / (cos(i + j+2) + pow(i + 2, 2));
 			}
 			else b[i][j] = 2 * i;
 		}
@@ -133,26 +171,24 @@ void FillMatrixNotMyFunction(double**b, int n)
 
 
 
-void DisplayMatrix(double**a, int n)
+
+void DisplayMatrix(double** a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cout << a[i][j] << " ";
-			if (j < n - 1) cout.width(20);
-			if(i==j&& i==0)cout.width(32);
+			cout.width(20);
+			cout << a[i][j];
 		}
 		cout << endl;
-
 	}
 }
 
-void ClearMemory(double**a, int m)
+inline void ClearMemory(double**a, int m)
 {
 	for (int i = 0; i < m; i++)
 	{
-
 		delete[] a[i];
 	}
 	delete[] a;
